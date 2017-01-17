1db606e352a66a21dda4b22d380d57e099edab18
Neeraj Shah <neerajvshah1@gmail.com>
2017-01-16 21:15:56 -0800

    Added clear functionality and state

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 var screenText = "";
+var state = "";
 $(document).ready(function(){
 	$('button').click(function(){
 		var value = $(this).attr("value");
 		switch(value){
 			case "ac":
+				state = "";
+				screenText = "";
 				break;
 			case "ce":
+				screenText = state;
 				break;
 			case "=":
 				screenText = eval(screenText);
+				state = screenText;
 				break;
 			default:
+				
+				if(!isNaN(value) && screenText === state)
+					screenText = "";
 				screenText += value;
 		}
+		console.log("ScreenText: " + screenText);
+		console.log("StateText: " + state);
 		$("#screen_text").text(screenText);
+		$("#state").text(state);
 	});
 });
